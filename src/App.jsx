3f5c7ed49ca282ac3f3d39adfe7d0a63033274c7
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-29 10:07:35 +0100

    initialized, and header component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CssHeader.css'
+
+const Header = () => {
+  return (
+    <nav>
+        <h1>M.Stafford</h1>
+        <h1>React Investment Calculator</h1>
+    </nav>
+  )
+}
+
+export default Header
