747268cc52a2a0a316c9e23f2a1d2a9cf8267fb7
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-29 13:23:03 +0100

    reset all inputs on negative number

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../components/CssUserInput.css'
 
 const UserInput = () => {
@@ -11,39 +11,45 @@ const UserInput = () => {
     })
 
     const callUserInput = (inputIdent, value) => {
-        if(userInput.startInvest < 0 || userInput.annualInvest < 0 || userInput.expectReturn < 0 ||userInput.yearsInvest < 0){
-            return (alert("Number can not be negative"))
+        if (userInput.startInvest < 0 || userInput.annualInvest < 0 || userInput.expectReturn < 0 || userInput.yearsInvest < 0) {
+            return (alert("Number can not be negative"),
+                    setUserInput({
+                            startInvest: 0,
+                            annualInvest: 0,
+                            expectReturn: 0,
+                            yearsInvest: 0
+            }))
         }
         setUserInput((prevState) => ({
-            ...prevState, 
-            [inputIdent]:value
+            ...prevState,
+            [inputIdent]: value
         }))
     }
 
     console.log(userInput)
 
-  return (
-    <section className='user-inputs'>
-    <div className='input-fields'>
-        <p>
-            <label>Starting Investment £</label>
-            <input type="number" value={userInput.startInvest} required onChange={(e)=>callUserInput("startInvest", e.target.value)}/>
-        </p>
-        <p>
-            <label>Annual Investment £</label>
-            <input type="number" required value={userInput.annualInvest} onChange={(e)=>callUserInput("annualInvest", e.target.value)}/>
-        </p>
-        <p>
-            <label>Expected Return %</label>
-            <input type="number" required value={userInput.expectReturn} onChange={(e)=>callUserInput("expectReturn", e.target.value)}/>
-        </p>
-        <p>
-            <label>Years to Invest</label>
-            <input type="number" required value={userInput.yearsInvest} onChange={(e)=>callUserInput("yearsInvest", e.target.value)}/>
-        </p>
-    </div>
-    </section>
-  )
+    return (
+        <section className='user-inputs'>
+            <div className='input-fields'>
+                <p>
+                    <label>Starting Investment £</label>
+                    <input type="number" value={userInput.startInvest} required onChange={(e) => callUserInput("startInvest", e.target.value)} />
+                </p>
+                <p>
+                    <label>Annual Investment £</label>
+                    <input type="number" required value={userInput.annualInvest} onChange={(e) => callUserInput("annualInvest", e.target.value)} />
+                </p>
+                <p>
+                    <label>Expected Return %</label>
+                    <input type="number" required value={userInput.expectReturn} onChange={(e) => callUserInput("expectReturn", e.target.value)} />
+                </p>
+                <p>
+                    <label>Years to Invest</label>
+                    <input type="number" required value={userInput.yearsInvest} onChange={(e) => callUserInput("yearsInvest", e.target.value)} />
+                </p>
+            </div>
+        </section>
+    )
 }
 
 export default UserInput
