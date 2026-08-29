156c699236d4d1c011900cb903795acae79b9781
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-29 13:11:47 +0100

    add css and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Header from './components/Header'
+import UserInput from './components/UserInput'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header user="M.Stafford"/>
+      <UserInput />
     </>
   )
 }
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import '../components/CssUserInput.css'
+
+const UserInput = () => {
+
+    const [userInput, setUserInput] = useState({
+        startInvest: 4000,
+        annualInvest: 1200,
+        expectReturn: 6,
+        yearsInvest: 35
+    })
+
+    const callUserInput = (inputIdent, value) => {
+        setUserInput((prevState) => ({
+            ...prevState, 
+            [inputIdent]:value
+        }))
+    }
+
+    console.log(userInput)
+
+  return (
+    <section className='user-inputs'>
+    <div className='input-fields'>
+        <p>
+            <label>Starting Investment £</label>
+            <input type="number" value={userInput.startInvest} required onChange={(e)=>callUserInput("startInvest", e.target.value)}/>
+        </p>
+        <p>
+            <label>Annual Investment £</label>
+            <input type="number" required value={userInput.annualInvest} onChange={(e)=>callUserInput("annualInvest", e.target.value)}/>
+        </p>
+        <p>
+            <label>Expected Return %</label>
+            <input type="number" required value={userInput.expectReturn} onChange={(e)=>callUserInput("expectReturn", e.target.value)}/>
+        </p>
+        <p>
+            <label>Years to Invest</label>
+            <input type="number" required value={userInput.yearsInvest} onChange={(e)=>callUserInput("yearsInvest", e.target.value)}/>
+        </p>
+    </div>
+    </section>
+  )
+}
+
+export default UserInput
