2441f6f0156259c7180f3cd36185269bcba3282e
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-30 10:35:19 +0100

    move userInput to App for passing to other components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,37 @@
 import './App.css'
 import Header from './components/Header'
 import UserInput from './components/UserInput'
+import { useState } from 'react'
 
 function App() {
   
+  const [userInput, setUserInput] = useState({
+          startInvest: 4000,
+          annualInvest: 1200,
+          expectReturn: 6,
+          yearsInvest: 35
+      })
+
+      const callUserInput = (inputIdent, value) => {
+        if (userInput.startInvest < 0 || userInput.annualInvest < 0 || userInput.expectReturn < 0 || userInput.yearsInvest < 0) {
+            return (alert("Number can not be negative"),
+                    setUserInput({
+                            startInvest: 0,
+                            annualInvest: 0,
+                            expectReturn: 0,
+                            yearsInvest: 0
+            }))
+        }
+        setUserInput((prevState) => ({
+            ...prevState,
+            [inputIdent]: value
+        }))
+    }
 
   return (
     <>
       <Header user="M.Stafford"/>
-      <UserInput />
+      <UserInput userInput={userInput} callUserInput={callUserInput}/>
     </>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,33 +1,10 @@
 import React, { useState } from 'react'
 import '../components/CssUserInput.css'
 
-const UserInput = () => {
-
-    const [userInput, setUserInput] = useState({
-        startInvest: 4000,
-        annualInvest: 1200,
-        expectReturn: 6,
-        yearsInvest: 35
-    })
-
-    const callUserInput = (inputIdent, value) => {
-        if (userInput.startInvest < 0 || userInput.annualInvest < 0 || userInput.expectReturn < 0 || userInput.yearsInvest < 0) {
-            return (alert("Number can not be negative"),
-                    setUserInput({
-                            startInvest: 0,
-                            annualInvest: 0,
-                            expectReturn: 0,
-                            yearsInvest: 0
-            }))
-        }
-        setUserInput((prevState) => ({
-            ...prevState,
-            [inputIdent]: value
-        }))
-    }
+const UserInput = ({userInput, callUserInput}) => {
 
     console.log(userInput)
-
+    
     return (
         <section className='user-inputs'>
             <div className='input-fields'>
