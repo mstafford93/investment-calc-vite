af33ed0d17de627053b440b3245653b6fac330e6
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-30 11:48:02 +0100

    adapt template css  and currency symbols

--- src/components/OutputData.jsx
+++ src/components/OutputData.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { calculateInvestmentResults } from "../util/investment.js"
+import './CssOutputData.css'
 
 const OutputData = ({ userInput }) => {
 
@@ -13,8 +14,29 @@ const OutputData = ({ userInput }) => {
     console.log(resultData)
 
     return (
-        <div></div>
-    )
+  <table>
+    <thead>
+      <tr>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Interest (Year)</th>
+        <th>Total Interest</th>
+        <th>Invested Capital</th>
+      </tr>
+    </thead>
+    <tbody>
+      {resultData.map((yearData, index) => (
+        <tr key={index}>
+          <td>{yearData.year}</td>
+          <td>£ {yearData.investmentValue.toFixed(2)}</td>
+          <td>£ {yearData.interest.toFixed(2)}</td>
+          <td>£ {yearData.totalInterest.toFixed(2)}</td>
+          <td>£ {yearData.investedCapital.toFixed(2)}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
 }
 
 export default OutputData
